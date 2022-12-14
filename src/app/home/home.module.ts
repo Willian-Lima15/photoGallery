199b2f53dd2e92088fd679bb7640761c977685b8
Willian Lima <william_slayer@hotmail.com>
2022-12-14 16:58:02 -0300

    Criado rotas filhas e componente login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
