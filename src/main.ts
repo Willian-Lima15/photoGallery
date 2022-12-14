0f8a7a62f0501e3233f046a4407f52d742778ed9
Willian Lima <william_slayer@hotmail.com>
2022-12-14 15:41:27 -0300

    Projeto iniciado e criado o home module

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { registerLicense } from '@syncfusion/ej2-base';
 
 if (environment.production) {
   enableProdMode();
 }
+
+registerLicense("ORg4AjUWIQA/Gnt2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0djW39XcHxRT2FaVUw=")
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
