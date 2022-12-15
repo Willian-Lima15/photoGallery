cfe4a5a0acd22d4ab87a4848ba85b7e5350ce229
Willian Lima <william_slayer@hotmail.com>
2022-12-15 11:32:33 -0300

    Criado e finalizado componente login, criado com componente novo usuário

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,17 +1,23 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
-  {path:'',
-  component:HomeComponent,
-  children: [
-    {path:'',
-    component:LoginComponent
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'novoUsuario', component: NovoUsuarioComponent
+      }
+    ]
   }
-  ]
-}
 ];
 
 @NgModule({
