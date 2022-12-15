2bceccabd9cb02ccf62e0f708ac96db7def26c43
Willian Lima <william_slayer@hotmail.com>
2022-12-15 08:35:54 -0300

    Criado modulo e serviço autenticação

---
+++ src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoService {
+
+  private url = 'http://localhost:3000/user/login';
+
+  constructor(
+    private http:HttpClient
+    ) { }
+
+    autentica(usuario:string, senha:string): Observable<any>{
+      return this.http.post(this.url, {userName: usuario, password: senha})
+    }
+}
