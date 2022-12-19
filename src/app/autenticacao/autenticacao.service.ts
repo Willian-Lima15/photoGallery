3ab85d0f799ccf54abdda05f7a76eb8b2a8aa720
Willian Lima <william_slayer@hotmail.com>
2022-12-19 14:54:51 -0300

    criado componente cabeçalho

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { UsuarioService } from './usuario/usuario.service';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,18 @@ export class AutenticacaoService {
 
   constructor(
     private http: HttpClient,
+    private usuarioService:UsuarioService
     ) { }
 
-    autenticar(usuario:string, senha:string): Observable<any>{
-      return this.http.post(this.url, {userName: usuario, password: senha});
+    autenticar(usuario:string, senha:string): Observable<HttpResponse<any>>{
+      return this.http.post(this.url,
+         {userName: usuario, password: senha},
+        {observe:'response'}
+        ).pipe(
+          tap((res)=> {
+            const authToken =  res.headers.get('x-access-token') ?? '';
+            this.usuarioService.salvaToken(authToken);
+          })
+        )
     }
 }
--- src/app/autenticacao/usuario/usuario.service.ts
+++ src/app/autenticacao/usuario/usuario.service.ts
@@ -10,10 +10,33 @@ import { BehaviorSubject } from 'rxjs';
 export class UsuarioService {
   private usuarioSubject = new BehaviorSubject<Usuario>({});
 
-  constructor(private tokenService: TokenService) { }
+  constructor(private tokenService: TokenService) {
+    if(this.tokenService.possuiToken()){
+      this.decodificaJwt();
+    }
+  }
 
   private decodificaJwt(){
     const token = this.tokenService.retornaToken();
     const usuario = jwtDecode(token) as Usuario;
+    this.usuarioSubject.next(usuario);
+  }
+
+  retornaUsuario(){
+    return this.usuarioSubject.asObservable();
+  }
+
+  salvaToken(token:string) {
+    this.tokenService.salvarToken(token);
+    this.decodificaJwt();
+  }
+
+  logout() {
+    this.tokenService.excluirToken();
+    this.usuarioSubject.next({});
+  }
+
+  estaLogado() {
+    return this.tokenService.possuiToken();
   }
 }
